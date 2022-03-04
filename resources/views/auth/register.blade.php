3a8e098f355bf0e0d2b3ee15e9cc76dea50397ba
ramsharan <ramsharan0230@gmail.com>
2022-03-04 12:18:43 +0545

    updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,9 +8,7 @@
         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
           <div class="d-flex justify-content-center py-4">
-            <a href="index.html" class="logo d-flex align-items-center w-auto">
-                <img src="{{ asset('assets-admin/images/ican-logo.jpg') }}" alt="logo" height="500px" width="100%">
-            </a>
+              <img src="{{ asset('assets-admin/images/ican-logo.jpg') }}" alt="logo" width="408" height="65" style="margin-bottom:-23px">
           </div><!-- End Logo -->
 
           <div class="card mb-3">
