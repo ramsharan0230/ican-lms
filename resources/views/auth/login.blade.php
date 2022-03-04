0d9226d6466b99411d057b768f5e21c7bd518def
ramsharan <ramsharan0230@gmail.com>
2022-03-04 12:12:17 +0545

    button color changed for login and register updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                     </div>
                 </div>
                 <div class="col-12">
-                    <button class="btn btn-primary w-100" type="submit">Login</button>
+                    <button class="btn btn-secondary w-100" type="submit">Login</button>
                 </div>
                 <div class="col-12">
                 <p class="small mb-0">Don't have account? <a href="{{ route('register') }}">Create an account</a></p>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,7 +58,7 @@
                   </div>
                 </div>
                 <div class="col-12">
-                  <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                  <button class="btn btn-secondary w-100" type="submit">Create Account</button>
                 </div>
                 <div class="col-12">
                   <p class="small mb-0">Already have an account? <a href="{{ route('login') }}">Log in</a></p>
